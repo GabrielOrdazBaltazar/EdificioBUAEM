7295d1cbb0f2b14d20f408297674bd6642ed2dce
GabrielOrdazBaltazar <tachyquellora123@gmail.com>
2023-11-24 12:10:16 -0600

    Se agrega funcionalidad 24/11/2023

--- EdificioB/src/main.java
+++ EdificioB/src/main.java
@@ -1,21 +1,21 @@
 public class main {
     public static void main(String[] args) {
-        //Vista Alta institucion
         /*
+        //Vista Alta institucion
         AltaInstitucionVista institucion  = new AltaInstitucionVista();
         institucion.setVisible(true);
         institucion.setLocationRelativeTo(null);
         */
         
-        //Vista Alta facultad
         /*
+        //Vista Alta facultad
         AltaFacultadVista facultad  = new AltaFacultadVista();
         facultad.setVisible(true);
         facultad.setLocationRelativeTo(null);
         */
         
-        //Vistas Alta Alumno
         /*
+        //Vistas Alta Alumno
         //Instancimos un objeto de la vista para dar de alta un alumno
         AltaAlumnoVista alumno = new AltaAlumnoVista();
         //llamamos a la vista para que sea visible
