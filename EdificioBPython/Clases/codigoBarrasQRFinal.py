1024174d0b52c23493986976d3a034c837ce73bd
Tachy <tachyquellora123@gmail.com>
2023-12-10 17:21:04 -0600

    Termina primer avance importante en lector qr con camara laptop 10/12/2023

--- EdificioBPython/Clases/codigoBarrasQRFinal.py
+++ EdificioBPython/Clases/codigoBarrasQRFinal.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
-import tkinter as tk
-from tkinter import messagebox
 
 # Función para decodificar códigos de barras y códigos QR en una imagen
 def decode_barcodes(frame):
@@ -13,36 +11,35 @@ def decode_barcodes(frame):
         barcode = barcodes[0]
         barcode_data = barcode.data.decode('utf-8')
         barcode_type = barcode.type
+        print(f'Código detectado: {barcode_data}, Tipo: {barcode_type}')
         return barcode_data, barcode_type, barcode
     else:
         # Si no se detecta ningún código
         return None, None, None
 
-# Función para mostrar el resultado en una ventana de Tkinter
-def show_result(barcode_data, barcode_type):
-    result_text = f'Código detectado: {barcode_data}, Tipo: {barcode_type}'
-    messagebox.showinfo('Resultado', result_text)
-
 # Inicializar la captura de la webcam
 cap = cv2.VideoCapture(0)
-
-# Crear una ventana de Tkinter
-root = tk.Tk()
-root.withdraw()  # Ocultar la ventana principal de Tkinter
 
 while True:
     # Capturar un frame de la webcam
     ret, frame = cap.read()
 
     # Decodificar códigos de barras y códigos QR
-    barcode_data, barcode_type, _ = decode_barcodes(frame)
+    barcode_data, barcode_type, barcode = decode_barcodes(frame)
 
-    # Mostrar el frame con el código de barras o código QR
-    cv2.imshow('Webcam', frame)
-
-    # Mostrar el resultado en la ventana de Tkinter
+    # Dibujar un rectángulo alrededor del código de barras o código QR
     if barcode_data is not None:
-        show_result(barcode_data, barcode_type)
+        rect_points = barcode.polygon
+        if rect_points and len(rect_points) == 4:
+            rect_points = [(point.x, point.y) for point in rect_points]
+
+            # Dibujar un polígono en lugar de un rectángulo
+            pts = np.array(rect_points, dtype=int)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(frame, [pts], isClosed=True, color=(0, 255, 255), thickness=2)
+
+    # Mostrar el frame con el código de barras o código QR y el rectángulo
+    cv2.imshow('Webcam', frame)
 
     # Romper el bucle si se presiona la tecla 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -51,5 +48,3 @@ while True:
 # Liberar los recursos
 cap.release()
 cv2.destroyAllWindows()
-
-
